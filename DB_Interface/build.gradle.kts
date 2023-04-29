490c7b1184df95497702b75729f212aa8ec8ae65
helloworld877 <mmghaly3@gmail.com>
2023-04-30 00:00:26 +0300

    added Database storing code

--- DB_Interface/build.gradle.kts
+++ DB_Interface/build.gradle.kts
@@ -12,6 +12,9 @@ repositories {
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("org.mongodb:mongodb-driver-sync:4.1.2")
+//    implementation("org.slf4j:slf4j-api:1.7.25")
+
 }
 
 tasks.test {
