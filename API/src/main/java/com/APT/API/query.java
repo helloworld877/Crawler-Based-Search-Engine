6943dc69a879f702e8278a4016b19d6da994fdc4
helloworld877 <mmghaly3@gmail.com>
2023-05-01 21:51:25 +0300

    Update query.java

--- API/src/main/java/com/APT/API/query.java
+++ API/src/main/java/com/APT/API/query.java
@@ -3,10 +3,20 @@ package com.APT.API;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 public class query {
     @GetMapping("/query")
-    public String query(){
-        return "Spring Boot Success";
+
+    public Map<String, String> query() {
+//        to make JSON Object
+        HashMap<String, String> map = new HashMap<>();
+        map.put("key", "value");
+        map.put("foo", "bar");
+        map.put("aa", "bb");
+        return map;
     }
+
 }
